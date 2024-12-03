86b39bcce0a20733f7958e1dad84604fb2e90e91
Hunter Parcells <hunterparcells@gmail.com>
2024-12-03 15:40:12 -0500

    feat: Form logic.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,64 @@
+import { useState } from 'react';
+
 function App() {
+  const [fullName, setFullName] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [address, setAddress] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState<string>('');
+
+  function submit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log({
+      fullName,
+      username,
+      email,
+      address,
+      phoneNumber
+    });
+  }
+
   return (
     <>
-      <p>Test.</p>
+      <form onSubmit={submit}>
+        <input
+          value={fullName}
+          onChange={(event) => {
+            setFullName(event.target.value);
+          }}
+          placeholder='Full Name'
+        />
+        <input
+          value={username}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+          placeholder='Username'
+        />
+        <input
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          placeholder='Email'
+        />
+        <input
+          value={address}
+          onChange={(event) => {
+            setAddress(event.target.value);
+          }}
+          placeholder='Address'
+        />
+        <input
+          value={phoneNumber}
+          onChange={(event) => {
+            setPhoneNumber(event.target.value);
+          }}
+          placeholder='Phone Number'
+        />
+        <button type='submit'>Submit</button>
+      </form>
     </>
   );
 }
